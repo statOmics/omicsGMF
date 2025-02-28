5e45c655a82fab49783816c6aed73b9d28451de5
Alexandre Segers <alexandre.segers@ugent.be>
2025-02-28 11:02:05 +0100

    add runSGD_rank

---
+++ R/plotSGD_rank.R
@@ -0,0 +1,78 @@
+#' Functions to create a scree plot for model selection.
+#'
+#' @param x Output of \link{sgdgmf.rank} or \link{runSGD_rank}.
+#' @param ... For the \code{screeplot_rank} generic, additional arguments to pass to specific methods.
+#' @param maxcomp Numeric scalar indicating the number of eigenvalues to plot.
+#' @param type Type of scree plot to make: choose between 'point', 'barplot' or 'lines.
+#' @param name String specifying the name to be used to obtain the rank object in the \code{\link{metadata}}.
+#' @details This function plots a screeplot based on the output of \link{runSGD_rank} or \link{sgdgmf.rank}.
+#'
+#' @return
+#' A \link{plot} object.
+#' @name plotSGD_rank
+#' @seealso
+#' \code{\link{runSGD_rank}}, to calculate the eigenvalues.
+#' @author Alexandre Segers
+#'
+#' @examples
+#' example_sce <- mockSCE(ncells = 200, ngenes = 100)
+#' example_sce <- runSGD_rank(example_sce,
+#'                          exprs_values="counts",
+#'                          family = poisson(),
+#'                          maxcomp = 10)
+#' head(metadata(example_sce)[["rank_SGD"]])
+#' plotSGD_rank(example_sce)
+NULL
+
+
+# adapted dfrom stats:::screeplot.default
+#' @importFrom grDevices dev.flush dev.hold
+#' @importFrom graphics axis barplot plot
+.screeplot_rank <- function (x,
+                             maxcomp = length(x$lambdas),
+                             type = c("point", "barplot", "lines"),
+                             ...)
+{
+
+
+    type <- match.arg(type)
+    pcs <- x$lambdas
+    xp <- seq_len(maxcomp)
+    dev.hold()
+    on.exit(dev.flush())
+    if (type == "barplot")
+        barplot(pcs[xp], names.arg = names(pcs[xp]),
+                ylab = "Eigenvalues", ...)
+    else if (type == "lines"){
+        plot(xp, pcs[xp], type = "b", axes = FALSE,
+             xlab = "", ylab = "Eigenvalues", ...)
+        axis(2)
+        axis(1, labels = names(pcs[xp]))
+    } else{
+        plot(xp, pcs[xp], type = "p", axes = FALSE,
+             xlab = "", ylab = "Eigenvalues", ...)
+        axis(2)
+        axis(1, labels = names(pcs[xp]))
+    }
+    invisible()
+}
+
+
+#' @export
+#' @rdname plotSGD_rank
+setMethod("screeplot_rank", "ANY", .screeplot_rank)
+
+
+#' @export
+#' @rdname plotSGD_rank
+#' @importFrom S4Vectors metadata
+plotSGD_rank <-  function(x, ..., name = "rank_SGD")
+{
+
+    screeplot_rank(x = S4Vectors::metadata(x)[[name]],
+                   ...)
+}
+
+
+
+
---
+++ R/runSGD_rank.R
@@ -0,0 +1,199 @@
+#' Perform an eigendecomposition for model selection based on a screeplot.
+#'
+#' @param x For \code{calculateSGD}, a numeric matrix of expression counts or mass spectrometry intensities where
+#' rows are features and columns are cells.
+#' Alternatively, a \linkS4class{SummarizedExperiment} or \linkS4class{SingleCellExperiment} containing such a matrix.
+#' For \code{runSGD}, a \linkS4class{SingleCellExperiment} object containing such a matrix.
+#' @param maxcomp Scalar indicating the maximal number of eigenvalues to compute.
+#' @param X Sample-level covariate matrix. Defaults to column of ones.
+#' @param Z Feature-level covariate matrix. Defaults to column of ones.
+#' @param family The distribution family that is used for the estimation of the parameters.
+#' @param offset offset matrix with same dimensions as x that is added to the linear predictor. Note that if family = poisson(), this should therefore be on the log-scale
+#' @param weights weight matrix with same dimensions as x that determines the weight of each observation.
+#' @param ntop Numeric scalar specifying the number of features with the highest variances to use for dimensionality reduction.
+#' Default uses all features.
+#' @param subset_row Vector specifying the subset of features to use for dimensionality reduction.
+#' This can be a character vector of row names, an integer vector of row indices or a logical vector.
+#' @param assay.type Integer scalar or string indicating which assay of \code{x} contains the values of interest.
+#' @param scale Logical scalar, should the expression values be standardized? Not recommended for non-Gaussian data.
+#' @param BSPARAM A \linkS4class{BiocSingularParam} object specifying which algorithm should be used to perform the PCA.
+#' This is used in \code{runPCA} to put all information in the sample latent factors.
+#' @param BPPARAM A \linkS4class{BiocParallelParam} object specifying whether the cross-validation
+#' should be parallelized. If BPPARAM$workers > 1 and control.cv$parallel and control.cv$nthreads are
+#' not specified, parallelization is enabled with nthreads = BPPARAM$workers.
+#' @param altexp String or integer scalar specifying an alternative experiment containing the input data.
+#' @param dimred String or integer scalar specifying the existing dimensionality reduction results to use.
+#' @param n_dimred Integer scalar or vector specifying the dimensions to use if \code{dimred} is specified.
+#' @param exprs_values Alias to \code{assay.type}.
+#' @param ... For the \code{calculateSGD} generic, additional arguments to pass to specific methods such as \link{sgdgmf.rank}.
+#' For the SummarizedExperiment and SingleCellExperiment methods, additional arguments to pass to the ANY method.
+#'
+#' For \code{runSGD}, additional arguments to pass to \code{calculateSGD}.
+#' @param name String specifying the name to be used to store the result in the \code{\link{metadata}} of the output.
+#' @param transposed Logical scalar, is \code{x} transposed with cells in rows?
+#' @param method rank selection method, see \link{sgdgmf.rank}.
+#' @param normalize if TRUE, standardize the residual matrix for each feature.
+#' @details
+#' sgdGMF uses sampling of the data to estimate the parameters, which can alter with different seeds. Also, cross-validation
+#' puts a random selection of values to missing. This means that the result will change slightly across different runs.
+#' For full reproducibility, users should call \code{\link{set.seed}} prior to running \code{runSGD} with such algorithms.
+#' (Note that this includes \code{BSPARAM=\link{bsparam}()}, which uses approximate algorithms by default.)
+#'
+#' @section Feature selection:
+#' This section is relevant if \code{x} is a numeric matrix with features in rows and cells in columns;
+#' or if \code{x} is a \linkS4class{SingleCellExperiment} and \code{dimred=NULL}.
+#' In the latter, the expression values are obtained from the assay specified by \code{assay.type}.
+#'
+#' The \code{subset_row} argument specifies the features to use for dimensionality reduction.
+#' The aim is to allow users to specify highly variable features to improve the signal/noise ratio,
+#' or to specify genes in a pathway of interest to focus on particular aspects of heterogeneity.
+#'
+#' If \code{subset_row=NULL}, the \code{ntop} features with the largest variances are used instead.
+#' We literally compute the variances from the expression values without considering any mean-variance trend,
+#' so often a more considered choice of genes is possible, e.g., with \pkg{scran} functions.
+#' Note that the value of \code{ntop} is ignored if \code{subset_row} is specified.
+#'
+#' If \code{scale=TRUE}, the expression values for each feature are standardized so that their variance is unity.
+#' This will also remove features with standard deviations below 1e-8.
+#'
+#'
+#' @return
+#' A list containing the eigenvalues. If a
+#' \linkS4class{SummarizedExperiment} or \linkS4class{SingleCellExperiment} was
+#' given as input, this is stored in the metadata of this object.
+#'
+#' @name runSGD_rank
+#' @seealso
+#' \code{\link[sgdGMF]{sgdgmf.rank}}, for the underlying calculations.
+#'
+#' @author Alexandre Segers
+#'
+#' @examples
+#' example_sce <- mockSCE(ncells = 200, ngenes = 100)
+#' example_sce <- runSGD_rank(example_sce,
+#'                          exprs_values="counts",
+#'                          family = poisson(),
+#'                          maxcomp = 10)
+#' head(metadata(example_sce)[["rank_SGD"]])
+#' plotSGD_rank(example_sce)
+NULL
+
+#' @importFrom MatrixGenerics colVars
+#' @importFrom DelayedArray DelayedArray
+#' @importFrom BiocParallel SerialParam bpstart bpstop
+#' @importFrom scuttle .bpNotSharedOrUp
+#' @importFrom sgdGMF sgdgmf.rank
+.calculate_sgd_rank <- function(x, family = poisson(), maxcomp = 100, ntop=NULL,
+                              X = NULL, Z = NULL, offset = NULL, weights = NULL,
+                              subset_row=NULL, scale=FALSE, transposed=FALSE,
+                              BSPARAM = bsparam(), BPPARAM = SerialParam(),
+                              method = "oht", normalize = FALSE, ...
+                              )
+    #TODO: ask Cristian upon default of method = "oht"
+{
+
+    # For DelayedArray's parallelized rowVars/colVars.
+    oldbp <- getAutoBPPARAM()
+    setAutoBPPARAM(BPPARAM)
+    on.exit(setAutoBPPARAM(oldbp))
+
+    if (.bpNotSharedOrUp(BPPARAM)) {
+        bpstart(BPPARAM)
+        on.exit(bpstop(BPPARAM), add=TRUE)
+    }
+
+    if (!transposed) {
+        out <- .get_mat_for_reddim(x, subset_row=subset_row, ntop=ntop, scale=scale, get.var=TRUE, family = family)
+        if(!is.null(Z)){
+            Z <- Z[c(rownames(x)) %in% colnames(out$x),, drop = FALSE]
+        }
+        if(!is.null(offset)){
+            if(all(dim(offset) == dim(x))){
+                offset <- t(offset[c(rownames(x)) %in% colnames(out$x),, drop = FALSE])
+            } else if(all(dim(offset) == dim(t(x)))){
+                offset <- offset[,c(rownames(x)) %in% colnames(out$x), drop = FALSE]
+            } else{
+                stop("Offset does not have equal dimensions compared to the assay used.")
+            }
+        }
+
+        if(!is.null(weights)){
+            if(all(dim(weights) == dim(x))){
+                weights <- t(weights[c(rownames(x)) %in% colnames(out$x),, drop = FALSE])
+            } else if(all(dim(weights) == dim(t(x)))){
+                weights <- weights[,c(rownames(x)) %in% colnames(out$x), drop = FALSE]
+            } else{
+                stop("Weights does not have equal dimensions compared to the assay used.")
+            }
+        }
+        x <- out$x
+        cv <- out$v
+    } else {
+        cv <- colVars(DelayedArray(x), useNames = TRUE)
+    }
+
+
+
+    if(BPPARAM$workers > 1){
+        parallel <- TRUE
+        nthreads <- BPPARAM$workers
+
+    }
+
+
+    rank <- sgdGMF::sgdgmf.rank(Y = x,
+                             X = X,
+                             Z = Z,
+                             maxcomp = maxcomp,
+                             family = family,
+                             weights = weights,
+                             offset = offset,
+                             method = method,
+                             normalize = normalize,
+                             ...)
+
+    return (rank)
+}
+
+#' @export
+#' @rdname runSGD_rank
+setMethod("calculateSGD_rank", "ANY", .calculate_sgd_rank)
+
+#' @export
+#' @rdname runSGD_rank
+#' @importFrom SummarizedExperiment assay
+#' @importFrom stats poisson
+setMethod("calculateSGD_rank", "SummarizedExperiment", function(x, ..., exprs_values="counts", assay.type=exprs_values, family = poisson())
+{
+    .checkfamily(assay(x, assay.type), family)
+    .calculate_sgd_rank(assay(x, assay.type), family, ...)
+})
+
+#' @export
+#' @rdname runSGD_rank
+#' @importFrom stats poisson
+setMethod("calculateSGD_rank", "SingleCellExperiment", function(x, ..., exprs_values="counts", dimred=NULL, n_dimred=NULL, assay.type=exprs_values, family = poisson())
+{
+    mat <- as.matrix(scater:::.get_mat_from_sce(x, assay.type=assay.type, dimred=dimred, n_dimred=n_dimred)) # TODO: check if needed & for dellayarray
+    .checkfamily(mat, family)
+    .calculate_sgd_rank(mat, family, transposed=!is.null(dimred), ...)
+})
+
+
+#' @export
+#' @rdname runSGD_rank
+#' @importFrom S4Vectors metadata<-
+#' @importFrom SingleCellExperiment altExp
+runSGD_rank <-  function(x, ..., altexp = NULL, name = "rank_SGD")
+{
+    if (!is.null(altexp)) {
+        y <- altExp(x, altexp)
+    } else {
+        y <- x
+    }
+    S4Vectors::metadata(x)[[name]] <- calculateSGD_rank(x, ...)
+    x
+}
+
+
+
